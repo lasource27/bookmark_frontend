a88e89689310220716c741840c4b355d4eac762f
Rui Liu <forward102910@gmail.com>
2021-10-31 23:40:25 +0000

    functionbar

--- src/Searchbar.js
+++ src/Searchbar.js
@@ -12,12 +12,12 @@ const Searchbar = ({search_bookmark}) => {
             <div className="dropdown">
                 <input type="text" className="searchbar_input" value={search_input} onChange={(e) => setSearch_input(e.target.value)} placeholder="enter your search" />
                 <ul className="dropdown_list">
-                    <li className="row-wrap">history1</li>
-                    <li className="row-wrap">history2</li> 
-                    <li className="row-wrap">history1</li>
-                    <li className="row-wrap">history2</li> 
-                    <li className="row-wrap">history1</li>
-                    <li className="row-wrap">history2</li> 
+                    <li className="row_wrap">history1</li>
+                    <li className="row_wrap">history2</li> 
+                    <li className="row_wrap">history1</li>
+                    <li className="row_wrap">history1</li>
+                    <li className="row_wrap">history2</li> 
+                    <li className="row_wrap">history1</li>
                 </ul>
             </div>
             <button className="searchbar_submit" onClick={() => search_bookmark(search_input)}><FontAwesomeIcon icon="search" /></button>
